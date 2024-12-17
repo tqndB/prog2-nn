1128672f4344f8f102e5055334411172ed09df82
Ouga Kogure <j3329ogb@sit.ac.jp>
2024-12-17 23:20:49 +0900

    デバイスの自動認識を追加

--- train.py
+++ train.py
@@ -1,79 +1,124 @@
+"""ニューラルネットワークの学習を行う"""
 import time
 
 import matplotlib.pyplot as plt
 
-
-
 import torch
 from torchvision import datasets
-import torchvision.trainforms.v2 as transforms
+import torchvision.transforms.v2 as transforms
 
 import models
 
+
+# GPU があれば 'cuda' なければ 'cpu' というデバイス名を設定
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# データセットの前処理関数
 ds_transform = transforms.Compose([
     transforms.ToImage(),
     transforms.ToDtype(torch.float32, scale=True)
 ])
+
+# データセットの読み込み
 ds_train = datasets.FashionMNIST(
     root='data',
-    train=False,
+    train=True,
     download=True,
     transform=ds_transform
 )
 
-batch_size = 64
-dataloader_train = torch.utils.data.DataLoader(
-    ds_test,
-    batch_size=batch_size
+ds_test = datasets.FashionMNIST(
+    root='data',
+    train=False,  # テスト用データセット
+    download=True,
+    transform=ds_transform
 )
 
-for image_batch, label_batch in dataloader_test:
+# ミニバッチのデータローダー
+bs = 64
+dataloader_train = torch.utils.data.DataLoader(
+    ds_train,
+    batch_size=bs,
+    shuffle=True
+)
+dataloader_test = torch.utils.data.DataLoader(
+    ds_test,
+    batch_size=bs,
+    shuffle=False
+)
+
+for image_batch, label_batch in dataloader_train:
     print(image_batch.shape)
-    print(image.shape)
+    print(label_batch.shape)
     break
 
-def test_accuracy(model, dataloader):
-    n_corrects = 0
-
-    model.eval()
-    for image_batch, label_batch in dataloader:
-        with torch.no_grad():
-            logits_batch = model(image_batch)
-
-        predict_batch = logits_batch.argmax(dim=1)
-        n_correct += (label_batch == predict_batch).sum().item()
-
-    accuracy = n_corrects / len(dataloader.datasets)
-
-    return accuracy  
-
+# モデルのインスタンスを作成
 model = models.MyModel()
 
-loss_fn = torch.nn.CrossEntropyLoss()
-
-learning_rate = 1e-3
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-acc_test = models.test_accuracy(model, dataloader_test)
+# 精度を計算する
+acc_train = models.test_accuracy(model, dataloader_train, device=device)
+print(f'train accuracy: {acc_train*100:.3f}%')
+acc_test = models.test_accuracy(model, dataloader_test, device=device)
 print(f'test accuracy: {acc_test*100:.3f}%')
 
-n_epochs = 5
+# ロス関数の選択
+loss_fn = torch.nn.CrossEntropyLoss()
 
+# 最適化手法の選択
+learning_rate = 0.003
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# criterion (規準) とも呼ぶ
 
+n_epochs = 20
 
-
-
-
-
-
+loss_train_history = []
+loss_test_history = []
+acc_train_history = []
+acc_test_history = []
 
 for k in range(n_epochs):
     print(f'epoch {k+1}/{n_epochs}', end=': ', flush=True)
-    loss_train = models.train(model, dataloader, loss_fn, optimizer)
-    print(f'train loss: {loss_train}')
-    loss_test = models.test(model, dataloader_test, loss_fn)
-    print(f'test loss: {loss_test}')
-    acc_train = models.test_accuracy(model, dataloader_test)
-    print(f'test accuracy: {acc_train*100:.2f}%')
-    acc_test = models.test_accuracy(model, dataloader_test)
-    print(f'test accuracy: {acc_test*100:.2f}%')
+
+    # 1 epoch の学習
+    time_start = time.time()
+    loss_train = models.train(model, dataloader_train, loss_fn, optimizer, device=device)
+    time_end = time.time()
+    loss_train_history.append(loss_train)
+    print(f'train loss: {loss_train:.3f} ({time_end-time_start:.1f}s)', end=', ')
+
+    time_start = time.time()
+    loss_test = models.test(model, dataloader_test, loss_fn, device=device)
+    time_end = time.time()
+    loss_test_history.append(loss_test)
+    print(f'test loss: {loss_test:.3f} ({time_end-time_start:.1f}s)')
+
+    # 精度を計算する
+    if (k+1) % 5 == 0:
+        time_start = time.time()
+        acc_train = models.test_accuracy(model, dataloader_train, device=device)
+        time_end = time.time()
+        acc_train_history.append(acc_train)
+        print(f'    train accuracy: {acc_train*100:.3f}% ({time_end-time_start:.1f}s)', end=', ')
+
+        time_start = time.time()
+        acc_test = models.test_accuracy(model, dataloader_test, device=device)
+        time_end = time.time()
+        acc_test_history.append(acc_test)
+        print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)')
+
+plt.plot(acc_train_history, label='train')
+plt.plot(acc_test_history, label='test')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+plt.plot(loss_train_history, label='train')
+plt.plot(loss_test_history, label='test')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.grid()
+plt.show()
